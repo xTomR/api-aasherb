8dc00758a2c5235a32448024acf7e45ec955f2d2
xTomR <96185908+xTomR@users.noreply.github.com>
2022-05-11 11:27:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ connectDB();
 
 const app = express();
 app.all("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://aasherb.netlify.app/");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://aasherb.netlify.app/login"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   res.header(
