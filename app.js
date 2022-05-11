588325408f163067e3261bed8ee17993fba15b90
xTomR <96185908+xTomR@users.noreply.github.com>
2022-05-11 11:02:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ dotenv.config({ path: "./config/config.env" });
 connectDB();
 
 const app = express();
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 // Scheduler
 scheduler();
