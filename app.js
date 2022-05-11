e151457903e8a3b10cc4d049d0e7931d791bfba7
xTomR <96185908+xTomR@users.noreply.github.com>
2022-05-11 16:41:50 -0400

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ let whitelist = [
   "https://www.aasherb.com",
   "http://www.aasherb.com/login",
   "https://www.aasherb.com/login",
+  "https://api.aasherb.com",
 ];
 var corsOptions = {
   origin: function (origin, callback) {
