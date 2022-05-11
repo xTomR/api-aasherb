0d3ec3f52037edf382741309c5fa22ef4b5d7367
xTomR <96185908+xTomR@users.noreply.github.com>
2022-05-11 11:06:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 app.all("*", function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://aasherb.netlify.app/login"
+    "https://aasherb.netlify.app/auth/login"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
