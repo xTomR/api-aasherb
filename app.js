a6a5ccf4d1b1b9a0ad107aa5087fdf36ad40ec64
xTomR <96185908+xTomR@users.noreply.github.com>
2022-05-13 11:37:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.use("/auth", require("./routes/auth"));
+app.use("/auth", cors(corsOptions), require("./routes/auth"));
 app.use("/api/deliveries", require("./routes/deliveries"));
 app.use("/api/addresses", require("./routes/addresses"));
 app.get("/api", (req, res) => {
