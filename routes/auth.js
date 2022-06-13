4bee3eebf5752577db980947165819884db97405
xTomR <96185908+xTomR@users.noreply.github.com>
2022-06-12 22:32:30 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,7 @@ router.get("/login", cors(corsOptions), (req, res) => {
   // } else {
   //   console.log(`error`);
   // }
-  res.send(req.user);
+  res.send("test");
 });
 
 router.get("/logout", cors(corsOptions), (req, res) => {
